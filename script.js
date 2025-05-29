34d2ea47aab1828a22519241c2b98a3dd3b66377
raquelvi16 <164515648+raquelvi16@users.noreply.github.com>
2025-05-29 10:48:52 -0300

    aula 3

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded'),function (){
+    const botaoDeAcessicibilidade = document.getElementById ('botao-acessibilidade')
+    const opcoesDeAcessibilidade = document.getElementById (opcoesDeAcessibilidade)
+
+    botaoDeAcessicibilidade.addEventListener
+}
